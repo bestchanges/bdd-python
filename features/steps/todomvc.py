db023a88c8701e1c0fa755643bb1f5c073b934d7
Egor Fedorov <egor.fedorov@gmail.com>
2019-09-24 16:58:42 +0300

    extend sample

--- features/steps/todomvc.py
+++ features/steps/todomvc.py
@@ -6,6 +6,28 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
 
+def find_tasks(browser: WebDriver) -> List[WebElement]:
+    elements = browser.find_elements_by_xpath(f'//ul[@class="todo-list"]/*')
+    return elements
+
+
+def find_task_by_name(browser: WebDriver, task_name) -> Optional[WebElement]:
+    tasks = find_tasks(browser)
+    for task in tasks:
+        found_label = task.find_element_by_xpath(f'//label[text()="{task_name}"]')
+        if found_label:
+            return task
+    return None
+
+
+def get_complete_control_for_task_li(task_li):
+    return task_li.find_element_by_xpath('//input[@class="toggle"]')
+
+
+def get_task_completed(task_li: WebElement):
+    return task_li.get_attribute('class')
+
+
 @step("list of tasks is empty")
 def step_impl(context):
     tasks = find_tasks(context.browser)
@@ -26,20 +48,6 @@ def step_impl(context, task_name):
     assert task
 
 
-def find_tasks(browser: WebDriver) -> List[WebElement]:
-    elements = browser.find_elements_by_xpath(f'//ul[@class="todo-list"]/*')
-    return elements
-
-
-def find_task_by_name(browser: WebDriver, task_name) -> Optional[WebElement]:
-    tasks = find_tasks(browser)
-    for task in tasks:
-        found_label = task.find_element_by_xpath(f'//label[text()="{task_name}"]')
-        if found_label:
-            return task
-    return None
-
-
 @given("user open webpage {url}")
 def step_impl(context, url):
     browser: WebDriver = context.browser
@@ -57,14 +65,6 @@ def step_impl(context, task_name):
     task_li = find_task_by_name(context.browser, task_name)
     complete_control = get_complete_control_for_task_li(task_li)
     complete_control.click()
-
-
-def get_complete_control_for_task_li(task_li):
-    return task_li.find_element_by_xpath('//input[@class="toggle"]')
-
-
-def get_task_completed(task_li: WebElement):
-    return task_li.get_attribute('class')
 
 
 @step("todo-item '{task_name}' is completed")
